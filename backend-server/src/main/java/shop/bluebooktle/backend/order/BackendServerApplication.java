63207adb5dfc8cc5835a149773e02d6d18b9c566
marf3927 <marf3927@gmail.com>
2025-05-02 13:42:52 +0900

    feat: 멀티모듈 구조 변경

---
+++ backend-server/src/main/java/shop/bluebooktle/backend/order/BackendServerApplication.java
@@ -0,0 +1,15 @@
+package shop.bluebooktle.backend.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class BackendServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendServerApplication.class, args);
+	}
+
+}
