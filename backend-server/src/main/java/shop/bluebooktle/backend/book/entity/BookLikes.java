ed6cf49f1d3b0239c33c33e73f835c3ae94331d7
soyeon MOON <oonsy99@naver.com>
2025-05-08 04:23:00 +0900

    fix(book): BookLikes 생성자 get() 수정

--- backend-server/src/main/java/shop/bluebooktle/backend/book/entity/BookLikes.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/entity/BookLikes.java
@@ -3,7 +3,6 @@ package shop.bluebooktle.backend.book.entity;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.MapsId;
