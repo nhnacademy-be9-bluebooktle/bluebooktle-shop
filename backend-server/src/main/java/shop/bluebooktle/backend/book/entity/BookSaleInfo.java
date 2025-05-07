f7563cde7e695549cd591d310c4f0cb9e2d3cf21
sondonghun <ehfk11115@gmail.com>
2025-05-07 13:26:33 +0900

    feat(Book) : Book CartBook 엔티티 연결

---
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/entity/BookSaleInfo.java
@@ -0,0 +1,28 @@
+package shop.bluebooktle.backend.book.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "book")
+public class BookSaleInfo {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "book_sale_info_id")
+	private Long bookSaleInfoId;
+
+	@Column(name = "book_id")
+	private Long bookId;
+}
