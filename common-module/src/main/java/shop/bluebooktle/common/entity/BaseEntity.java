44e0bb8b09f2d69896f0d22a35470de1443b955e
marf3927 <marf3927@gmail.com>
2025-05-07 10:51:24 +0900

    feat(Common-module): BaseEntity 추가

---
+++ common-module/src/main/java/shop/bluebooktle/common/entity/BaseEntity.java
@@ -0,0 +1,23 @@
+package shop.bluebooktle.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+	@CreatedDate
+	@Column(name = "created_at", nullable = false, updatable = false)
+	private LocalDateTime createdAt;
+
+	@Column(name = "deleted_at")
+	private LocalDateTime deletedAt;
+}
