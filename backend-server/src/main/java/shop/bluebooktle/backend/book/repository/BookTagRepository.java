4f6646ad44552b163727e9b8112be6e8e2f682fc
soyeon MOON <oonsy99@naver.com>
2025-05-09 03:27:15 +0900

    feat(book): repository 파일 추가 (충돌 해결)

--- backend-server/src/main/java/shop/bluebooktle/backend/book/repository/BookTagRepository.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/repository/BookTagRepository.java
@@ -1,8 +1,25 @@
 package shop.bluebooktle.backend.book.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import shop.bluebooktle.backend.book.entity.Book;
 import shop.bluebooktle.backend.book.entity.BookTag;
+import shop.bluebooktle.backend.book.entity.Tag;
 
 public interface BookTagRepository extends JpaRepository<BookTag, Long> {
+
+	// 특정 도서의 모든 태그 조회
+	@Query("SELECT bt.tag FROM BookTag bt WHERE bt.book = :book")
+	List<Tag> findTagsByBook(@Param("book") Book book);
+
+	// 특정 태그의 모든 도서 조회
+	@Query("SELECT bt.book FROM BookTag bt WHERE bt.tag = :tag")
+	List<Book> findBooksByTag(@Param("tag") Tag tag);
+
+	// 특정 도서의 특정 태그 연결 유무 조회 (연결 시 true)
+	boolean existsByBookAndTag(Book book, Tag tag);
 }
--- backend-server/src/main/java/shop/bluebooktle/backend/book/repository/ImgRepository.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/repository/ImgRepository.java
@@ -1,11 +1,15 @@
 package shop.bluebooktle.backend.book.repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import shop.bluebooktle.backend.book.entity.Img;
 
 public interface ImgRepository extends JpaRepository<Img, Long> {
-	Optional<Img> findByImgUrl(String imgUrl);
+
+	// 특정 이미지 (url) 삭제 여부 조회 (삭제면 true)
+	boolean existsByImgUrlAndDeletedAtIsNotNull(String imgUrl);
+
+	// 이미지 (url) 중복 조회 (이미 url이 존재하면 true)
+	boolean existsByImgUrl(String imgUrl);
+
 }
