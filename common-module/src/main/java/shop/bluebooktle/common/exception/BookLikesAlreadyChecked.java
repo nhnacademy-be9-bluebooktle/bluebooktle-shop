da8f404f0c9c3f488856b72e302ae86c31ef6853
sslynn22 <sslynn022@gmail.com>
2025-05-08 15:37:34 +0900

    feat(book): 도서 좋아요 Service 추가

---
+++ common-module/src/main/java/shop/bluebooktle/common/exception/BookLikesAlreadyChecked.java
@@ -0,0 +1,7 @@
+package shop.bluebooktle.common.exception;
+
+public class BookLikesAlreadyChecked extends RuntimeException {
+	public BookLikesAlreadyChecked(String message) {
+		super(message);
+	}
+}
