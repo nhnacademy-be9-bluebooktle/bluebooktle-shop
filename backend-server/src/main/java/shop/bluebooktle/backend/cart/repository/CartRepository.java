0df88ff8a8fc94d5aaeea50d83b7170e6f9ce4ef
이용준 <junyboy124@gmail.com>
2025-05-08 20:22:47 +0900

    feat(Cart/CartBook) : Repository/Service/Dto/Redis 추가

---
+++ backend-server/src/main/java/shop/bluebooktle/backend/cart/repository/CartRepository.java
@@ -0,0 +1,18 @@
+package shop.bluebooktle.backend.cart.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import shop.bluebooktle.backend.cart.entity.Cart;
+import shop.bluebooktle.common.entity.User;
+
+public interface CartRepository extends JpaRepository<Cart, Long> {
+
+	@EntityGraph(attributePaths = {"cartBooks", "cartBooks.book"})
+	Optional<Cart> findByUser(User user);
+
+	boolean existsByUser(User user);
+
+}
