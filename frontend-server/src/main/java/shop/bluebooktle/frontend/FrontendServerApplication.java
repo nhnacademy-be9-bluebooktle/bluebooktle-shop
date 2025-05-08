18d30ce04855ea7f4cd8a6802f29d05ef6e5e60e
gunsurius <68006905+Shin-Gunsu@users.noreply.github.com>
2025-05-08 16:23:03 +0900

    [#82][Infra][eureka] - 유레카 등록 (#85)
    * feat: add eureka client
    
    * refactor: 각 ã모듈 유레카 클라이언트 설정

--- frontend-server/src/main/java/shop/bluebooktle/frontend/FrontendServerApplication.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/FrontendServerApplication.java
@@ -2,8 +2,10 @@ package shop.bluebooktle.frontend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FrontendServerApplication {
 
 	public static void main(String[] args) {
