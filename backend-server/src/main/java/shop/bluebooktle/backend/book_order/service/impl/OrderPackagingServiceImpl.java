8c523b2f67aaf27ac342588f79418d6dc2176723
Shin gunsu <gunsu980@gmail.com>
2025-05-09 22:48:02 +0900

    fix : add @service annotation to service classes

--- backend-server/src/main/java/shop/bluebooktle/backend/book_order/service/impl/OrderPackagingServiceImpl.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/book_order/service/impl/OrderPackagingServiceImpl.java
@@ -1,6 +1,9 @@
 package shop.bluebooktle.backend.book_order.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import shop.bluebooktle.backend.book_order.service.OrderPackagingService;
 
+@Service
 public class OrderPackagingServiceImpl implements OrderPackagingService {
 }
