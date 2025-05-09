6f888ab1913389c41113a8a946d13cd0bb71d92f
marf3927 <marf3927@gmail.com>
2025-05-09 13:09:19 +0900

    fix(user): user entity import 수정 (#96)

--- backend-server/src/main/java/shop/bluebooktle/backend/cart/service/CartService.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/cart/service/CartService.java
@@ -2,7 +2,7 @@ package shop.bluebooktle.backend.cart.service;
 
 import shop.bluebooktle.backend.book.entity.Book;
 import shop.bluebooktle.backend.cart.entity.Cart;
-import shop.bluebooktle.common.entity.User;
+import shop.bluebooktle.common.entity.auth.User;
 
 public interface CartService {
 
