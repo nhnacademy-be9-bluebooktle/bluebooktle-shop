82345b6d5a18702ff5f5255e5c6052e0ad3b9d88
gunsu <gunsu980@gmail.com>
2025-05-09 13:11:04 +0900

    fix: test

--- frontend-server/src/main/java/shop/bluebooktle/frontend/FrontendServerApplication.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/FrontendServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class FrontendServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(FrontendServerApplication.class, args);
 	}
--- gateway-server/src/main/java/shop/bluebooktle/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/shop/bluebooktle/gateway/GatewayServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
