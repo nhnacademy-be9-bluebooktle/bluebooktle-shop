546a5c5c9f93cd80b86560c3d608f552e1dedc90
gunsu <gunsu980@gmail.com>
2025-05-09 15:06:28 +0900

    chore : update workflow yml

--- gateway-server/src/main/java/shop/bluebooktle/gateway/GatewayServerApplication.java
+++ gateway-server/src/main/java/shop/bluebooktle/gateway/GatewayServerApplication.java
@@ -7,9 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class GatewayServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServerApplication.class, args);
 	}
-
 }
