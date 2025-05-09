8b9a791b121db6b08536d7c4b3ea5f441af596e0
gunsurius <68006905+Shin-Gunsu@users.noreply.github.com>
2025-05-09 15:39:25 +0900

    [#86 ][Infra] - 무중단 배포 (#100)
    * feat: 액츄ㅔ이터 엔드포인트추ã가
    
    * feat: update workflows maven.yml
    
    * chore : update workflow yml

--- backend-server/src/main/java/shop/bluebooktle/backend/BackendServerApplication.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/BackendServerApplication.java
@@ -12,7 +12,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 	"shop.bluebooktle.common.entity"
 })
 public class BackendServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServerApplication.class, args);
 	}
