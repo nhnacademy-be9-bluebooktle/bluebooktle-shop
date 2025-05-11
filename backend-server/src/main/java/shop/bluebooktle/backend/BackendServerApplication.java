26d5239287754cb8b70b5ee520e1b107715b2a44
김재혁 <rlawog@nate.com>
2025-05-11 19:31:36 +0900

    refactor(Coupon): coupon 등록,조회 간단 오류 수정

--- backend-server/src/main/java/shop/bluebooktle/backend/BackendServerApplication.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/BackendServerApplication.java
@@ -4,13 +4,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"shop.bluebooktle.backend",
+	"shop.bluebooktle.common"
+})
 @EnableFeignClients
 @EntityScan(basePackages = {
 	"shop.bluebooktle.backend",
 	"shop.bluebooktle.common.entity"
 })
+
+@EnableJpaAuditing
 public class BackendServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServerApplication.class, args);
