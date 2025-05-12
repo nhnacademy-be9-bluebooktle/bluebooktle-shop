f82922f6e93de36de0ff4302af7e360bfe32e88a
gunsu <gunsu980@gmail.com>
2025-05-12 17:10:34 +0900

    feat: add gateway config

---
+++ gateway-server/src/main/java/shop/bluebooktle/gateway/config/RouterConfig.java
@@ -0,0 +1,20 @@
+package shop.bluebooktle.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouterConfig {
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("backend-server", r -> r.path("/api/**")
+				.uri("lb://backend-server"))
+			.route("frontend-server", r -> r.path("/**")
+				.uri("lb://frontend-server"))
+			.build();
+	}
+}
