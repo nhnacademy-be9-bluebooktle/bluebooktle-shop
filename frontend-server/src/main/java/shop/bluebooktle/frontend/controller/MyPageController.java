d7140f98545d04a726a0cb4d30beeeae2ef09893
marf3927 <marf3927@gmail.com>
2025-05-12 16:24:50 +0900

    feat(front): 마이페이지(내정보, 주문, 주소,포인트, 쿠폰, 리뷰, 좋아요) 추가

---
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/controller/MyPageController.java
@@ -0,0 +1,60 @@
+package shop.bluebooktle.frontend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MyPageController {
+
+	@GetMapping
+	public String myPageDefault() {
+		return "redirect:/mypage/profile";
+	}
+
+	@GetMapping("/profile")
+	public String userProfilePage() {
+		return "mypage/profile";
+	}
+
+	@GetMapping("/orders")
+	public String userOrdersPage() {
+		return "mypage/order_list";
+	}
+
+	@GetMapping("/orders/{orderId}")
+	public String orderDetailPage(@PathVariable String orderId) {
+		System.out.println("Requesting order detail for (Mock): " + orderId);
+		return "mypage/order_detail";
+	}
+
+	@GetMapping("/addresses")
+	public String userAddressesPage() {
+		return "mypage/address_list";
+	}
+
+	@GetMapping("/points")
+	public String userPointsPage() {
+		return "mypage/point_log";
+	}
+
+	@GetMapping("/coupons")
+	public String userCouponsPage() {
+
+		return "mypage/coupon_list";
+	}
+
+	@GetMapping("/reviews")
+	public String userReviewsPage() {
+
+		return "mypage/review_list";
+	}
+
+	@GetMapping("/likes")
+	public String userWishlistPage() {
+
+		return "mypage/likes_list";
+	}
+}
