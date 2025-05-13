8fd49417b891c168593908932d0cac7f62de14cc
Yongjun Lee <59440910+yoplenople@users.noreply.github.com>
2025-05-13 17:05:04 +0900

    feat(Order) : 마이페이지 주문내역 조회 Repository, Service 작성 (#165)
    Co-authored-by: gunsurius <68006905+Shin-Gunsu@users.noreply.github.com>

--- backend-server/src/main/java/shop/bluebooktle/backend/order/service/OrderService.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/order/service/OrderService.java
@@ -1,4 +1,20 @@
 package shop.bluebooktle.backend.order.service;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.data.domain.Pageable;
+
+import shop.bluebooktle.backend.order.entity.Order;
+import shop.bluebooktle.common.domain.OrderStatus;
+import shop.bluebooktle.common.entity.auth.User;
+
 public interface OrderService {
+
+	List<Order> getUserOrders(User user, OrderStatus status, LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+	Order getOrderByOrderKey(UUID orderKey);
+
+	void updateOrderStatus(Long orderId, OrderStatus newStatus);
 }
