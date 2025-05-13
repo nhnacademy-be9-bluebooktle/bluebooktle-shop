66ac8f214a115588a77b8f5623f6e0732dbcfe71
Yongjun Lee <59440910+yoplenople@users.noreply.github.com>
2025-05-13 17:05:04 +0900

    feat(Order) : 마이페이지 주문내역 조회 Repository, Service 작성 (#165)
    Co-authored-by: gunsurius <68006905+Shin-Gunsu@users.noreply.github.com>

--- backend-server/src/main/java/shop/bluebooktle/backend/order/repository/OrderStateRepository.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/order/repository/OrderStateRepository.java
@@ -1,8 +1,14 @@
 package shop.bluebooktle.backend.order.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import shop.bluebooktle.backend.order.entity.OrderState;
+import shop.bluebooktle.common.domain.OrderStatus;
 
 public interface OrderStateRepository extends JpaRepository<OrderState, Long> {
+
+	Optional<OrderState> findByState(OrderStatus state); // 상태 enum → 엔티티로 변환
+
 }
