24457d7a67c972d08c2db99974c77e9530110648
chossa <sua6458@gmail.com>
2025-05-20 14:39:27 +0900

    fix(도서): dto common-module 리팩터링

--- backend-server/src/main/java/shop/bluebooktle/backend/book/dto/response/AladinBookItemBySearch.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/dto/response/AladinBookItemBySearch.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.Getter;
-import shop.bluebooktle.backend.book.dto.response.AladinAuthorResponse;
 
 @Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
