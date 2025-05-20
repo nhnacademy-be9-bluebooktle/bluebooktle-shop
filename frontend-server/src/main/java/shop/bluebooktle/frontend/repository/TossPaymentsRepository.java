9957cb530b14927ddb202ffabc5b0defb5a02289
gunsurius <68006905+Shin-Gunsu@users.noreply.github.com>
2025-05-20 11:28:15 +0900

    [#126][[BACK][결제][Toss] - 토스페이먼트 추가 (#238)
    * feat: add tosspayment service
    
    * feat: add valid
    
    * fix : bookRegisterserivce에서 오류나는 부분 제거함
    
    * fix: toss url
    
    * fix: rename payment reponse package -> response

---
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/repository/TossPaymentsRepository.java
@@ -0,0 +1,22 @@
+package shop.bluebooktle.frontend.repository;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import shop.bluebooktle.common.dto.payment.request.TossConfirmRequest;
+import shop.bluebooktle.common.dto.payment.response.TossConfirmResponse;
+import shop.bluebooktle.frontend.config.TossFeignConfig;
+
+@FeignClient(
+	name = "tossPaymentsClient",
+	url = "https://api.tosspayments.com/v1",
+	configuration = TossFeignConfig.class
+)
+public interface TossPaymentsRepository {
+
+	@PostMapping(value = "/payments/confirm", consumes = MediaType.APPLICATION_JSON_VALUE)
+	TossConfirmResponse confirmPayment(@RequestBody TossConfirmRequest request);
+
+}
