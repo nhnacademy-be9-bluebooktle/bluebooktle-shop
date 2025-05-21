f4c1d82ecaddd5717b2878ef9bf894b453577189
chossa <sua6458@gmail.com>
2025-05-21 21:38:35 +0900

    feat(도서): 카테고리, 작가 관리자 페이지 구현

---
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/service/CategoryService.java
@@ -0,0 +1,37 @@
+package shop.bluebooktle.frontend.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import shop.bluebooktle.common.dto.book.request.CategoryRegisterRequest;
+import shop.bluebooktle.common.dto.book.request.CategoryUpdateRequest;
+import shop.bluebooktle.common.dto.book.request.RootCategoryRegisterRequest;
+import shop.bluebooktle.common.dto.book.response.CategoryResponse;
+import shop.bluebooktle.common.dto.book.response.CategoryTreeResponse;
+
+public interface AdminCategoryService {
+
+	// 전체 카테고리 트리 조회
+	List<CategoryTreeResponse> searchAllCategoriesTree();
+
+	// 카테고리 리스트 조회
+	Page<CategoryResponse> getCategories(int page, int size, String searchKeyword);
+
+	// 카테고리 조회
+	CategoryResponse getCategory(Long id);
+
+	// 카테고리 조회
+	List<CategoryResponse> getAllCategories();
+
+	// 카테고리 생성
+	void addRootCategory(RootCategoryRegisterRequest request);
+
+	void addCategory(Long parentCategoryId, CategoryRegisterRequest request);
+
+	// 카테고리 수정
+	void updateCategory(Long parentCategoryId, CategoryUpdateRequest request);
+
+	// 카테고리 삭제
+	void deleteCategory(Long parentCategoryId);
+}
---
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,80 @@
+package shop.bluebooktle.frontend.service.impl;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import shop.bluebooktle.common.dto.book.request.CategoryRegisterRequest;
+import shop.bluebooktle.common.dto.book.request.CategoryUpdateRequest;
+import shop.bluebooktle.common.dto.book.request.RootCategoryRegisterRequest;
+import shop.bluebooktle.common.dto.book.response.CategoryResponse;
+import shop.bluebooktle.common.dto.book.response.CategoryTreeResponse;
+import shop.bluebooktle.common.dto.common.JsendResponse;
+import shop.bluebooktle.common.dto.common.PaginationData;
+import shop.bluebooktle.frontend.repository.CategoryRepository;
+import shop.bluebooktle.frontend.service.AdminCategoryService;
+
+@Service
+@RequiredArgsConstructor
+public class AdminCategoryServiceImpl implements AdminCategoryService {
+
+	private final CategoryRepository categoryRepository;
+
+	@Override
+	public List<CategoryTreeResponse> searchAllCategoriesTree() {
+		JsendResponse<List<CategoryTreeResponse>> response = categoryRepository.allCategoriesTree();
+		return response.data();
+	}
+
+	@Override
+	public Page<CategoryResponse> getCategories(int page, int size, String searchKeyword) {
+		Pageable pageable = PageRequest.of(page, size);
+
+		String keyword = null;
+		if (searchKeyword != null && !searchKeyword.isBlank()) {
+			keyword = searchKeyword;
+		}
+
+		JsendResponse<PaginationData<CategoryResponse>> response = categoryRepository.allCategories(page, size,
+			keyword);
+		PaginationData<CategoryResponse> data = response.data();
+		List<CategoryResponse> categories = data.getContent();
+		return new PageImpl<>(categories, pageable, data.getTotalElements());
+	}
+
+	@Override
+	public CategoryResponse getCategory(Long id) {
+		return categoryRepository.getCategory(id).data();
+	}
+
+	@Override
+	public List<CategoryResponse> getAllCategories() {
+		JsendResponse<List<CategoryResponse>> allCategories = categoryRepository.getAllCategories();
+		return allCategories.data();
+	}
+
+	@Override
+	public void addRootCategory(RootCategoryRegisterRequest request) {
+		categoryRepository.addRootCategory(request);
+	}
+
+	@Override
+	public void addCategory(Long parentCategoryId, CategoryRegisterRequest request) {
+		categoryRepository.addCategory(parentCategoryId, request);
+	}
+
+	@Override
+	public void updateCategory(Long categoryId, CategoryUpdateRequest request) {
+		categoryRepository.updateCategory(categoryId, request);
+	}
+
+	@Override
+	public void deleteCategory(Long parentCategoryId) {
+		categoryRepository.deleteCategory(parentCategoryId);
+	}
+}
