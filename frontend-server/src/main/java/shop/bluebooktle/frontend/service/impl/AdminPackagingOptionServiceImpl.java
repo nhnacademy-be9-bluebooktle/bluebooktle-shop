affbbfb801098010954acc3063bfb2bcbc3d5fd9
sslynn22 <sslynn022@gmail.com>
2025-05-22 16:31:47 +0900

    feat(book_order): 포장 옵션 프론트 작성

---
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/service/impl/AdminPackagingOptionServiceImpl.java
@@ -0,0 +1,54 @@
+package shop.bluebooktle.frontend.service.impl;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import shop.bluebooktle.common.dto.book_order.request.PackagingOptionRequest;
+import shop.bluebooktle.common.dto.book_order.response.PackagingOptionInfoResponse;
+import shop.bluebooktle.common.dto.common.PaginationData;
+import shop.bluebooktle.frontend.repository.AdminPackagingOptionRepository;
+import shop.bluebooktle.frontend.service.AdminPackagingOptionService;
+
+@Service
+@RequiredArgsConstructor
+public class AdminPackagingOptionServiceImpl implements AdminPackagingOptionService {
+	private final AdminPackagingOptionRepository PackagingOptionRepository;
+
+	@Override
+	public Page<PackagingOptionInfoResponse> getPackingOptions(int page, int size, String searchKeyword) {
+		Pageable pageable = PageRequest.of(page, size);
+
+		String keyword = null;
+		if (searchKeyword != null && !searchKeyword.isBlank()) {
+			keyword = searchKeyword;
+		}
+
+		PaginationData<PackagingOptionInfoResponse> data = PackagingOptionRepository.getPackagingOptions(page, size,
+			keyword);
+		return new PageImpl<>(data.getContent(), pageable, data.getTotalElements());
+	}
+
+	@Override
+	public PackagingOptionInfoResponse getPackingOption(Long id) {
+		return null;
+	}
+
+	@Override
+	public void deletePackingOption(Long id) {
+
+	}
+
+	@Override
+	public void updatePackingOption(Long id, PackagingOptionRequest request) {
+
+	}
+
+	@Override
+	public void createPackingOption(PackagingOptionRequest request) {
+
+	}
+}
