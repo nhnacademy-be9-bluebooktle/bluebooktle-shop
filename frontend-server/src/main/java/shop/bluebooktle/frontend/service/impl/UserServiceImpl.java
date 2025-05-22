8b40d4c54cc22bcee8163928379462f88964fc40
marf3927 <marf3927@gmail.com>
2025-05-22 22:08:27 +0900

    cmt

--- frontend-server/src/main/java/shop/bluebooktle/frontend/service/impl/UserServiceImpl.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/service/impl/UserServiceImpl.java
@@ -6,7 +6,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import shop.bluebooktle.common.dto.user.request.UserUpdateRequest;
 import shop.bluebooktle.common.dto.user.response.UserResponse;
-import shop.bluebooktle.frontend.config.retry.RetryWithTokenRefresh;
 import shop.bluebooktle.frontend.repository.UserRepository;
 import shop.bluebooktle.frontend.service.UserService;
 
@@ -17,13 +16,11 @@ public class UserServiceImpl implements UserService {
 	private final UserRepository userRepository;
 
 	@Override
-	@RetryWithTokenRefresh
 	public UserResponse getMe() {
 		return userRepository.getMe();
 	}
 
 	@Override
-	@RetryWithTokenRefresh
 	public void updateUser(long id, UserUpdateRequest userUpdateRequest) {
 		userRepository.updateUser(id, userUpdateRequest);
 	}
