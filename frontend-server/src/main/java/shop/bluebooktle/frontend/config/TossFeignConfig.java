0f14f91a9c9bb2e5d1718d36e72a49087a2f2375
marf3927 <marf3927@gmail.com>
2025-05-22 10:33:00 +0900

    [FRONT][FIX] - FIx feign client configuration (#268)
    * fix(Security): fix configuration
    
    * fix(Security): fix configuration

--- frontend-server/src/main/java/shop/bluebooktle/frontend/config/TossFeignConfig.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/config/TossFeignConfig.java
@@ -5,12 +5,9 @@ import java.util.Base64;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import feign.RequestInterceptor;
-import feign.RequestTemplate;
 
-@Configuration
 public class TossFeignConfig {
 
 	@Value("${toss.secret-key}")
@@ -18,14 +15,11 @@ public class TossFeignConfig {
 
 	@Bean
 	public RequestInterceptor tossAuthInterceptor() {
-		return new RequestInterceptor() {
-			@Override
-			public void apply(RequestTemplate template) {
-				String token = Base64.getEncoder()
-					.encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
-				template.header("Authorization", "Basic " + token);
-				template.header("Content-Type", "application/json");
-			}
+		return template -> {
+			String token = Base64.getEncoder()
+				.encodeToString((secretKey + ":").getBytes(StandardCharsets.UTF_8));
+			template.header("Authorization", "Basic " + token);
+			template.header("Content-Type", "application/json");
 		};
 	}
 }
