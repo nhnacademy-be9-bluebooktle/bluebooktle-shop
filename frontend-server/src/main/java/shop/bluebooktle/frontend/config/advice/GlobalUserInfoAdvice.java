bcb896671cc752e0beaeafcbb71da05a595c871e
marf3927 <marf3927@gmail.com>
2025-05-22 22:49:53 +0900

    cmt

--- frontend-server/src/main/java/shop/bluebooktle/frontend/config/advice/GlobalUserInfoAdvice.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/config/advice/GlobalUserInfoAdvice.java
@@ -14,23 +14,23 @@ import shop.bluebooktle.frontend.util.JwtPayloadUtil;
 @ControllerAdvice
 public class GlobalUserInfoAdvice {
 
-	private static final String CLAIM_USER_ID = "sub";
+	private static final String CLAIM_USER_ID = "userId";
 	private static final String CLAIM_USER_NICKNAME = "userNickname";
 	private static final String CLAIM_USER_TYPE = "userType";
 
 	@ModelAttribute
 	public void addUserInfoToModel(Model model,
-		@CookieValue(name = CookieTokenUtil.REFRESH_TOKEN_COOKIE_NAME, required = false) String accessToken) {
+		@CookieValue(name = CookieTokenUtil.REFRESH_TOKEN_COOKIE_NAME, required = false) String refreshToken) {
 
 		boolean isLoggedIn = false;
 		Long userId = null;
 		String userNickname = null;
 		String userType = null;
 
-		if (StringUtils.hasText(accessToken)) {
-			Map<String, Object> claims = JwtPayloadUtil.getPayloadClaims(accessToken);
+		if (StringUtils.hasText(refreshToken)) {
+			Map<String, Object> claims = JwtPayloadUtil.getPayloadClaims(refreshToken);
 			isLoggedIn = true;
-
+			userId = JwtPayloadUtil.getClaim(claims, "sub", Long.class);
 			userNickname = JwtPayloadUtil.getClaim(claims, CLAIM_USER_NICKNAME, String.class);
 			userType = JwtPayloadUtil.getClaim(claims, CLAIM_USER_TYPE, String.class);
 		}
