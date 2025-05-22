b685e8bb3d95cf6ef165d22cef6b975852a2125c
marf3927 <marf3927@gmail.com>
2025-05-22 22:08:27 +0900

    cmt

--- frontend-server/src/main/java/shop/bluebooktle/frontend/repository/UserRepository.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/repository/UserRepository.java
@@ -9,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestBody;
 import shop.bluebooktle.common.dto.user.request.UserUpdateRequest;
 import shop.bluebooktle.common.dto.user.response.UserResponse;
 import shop.bluebooktle.frontend.config.feign.FeignGlobalConfig;
+import shop.bluebooktle.frontend.config.retry.RetryWithTokenRefresh;
 
 @FeignClient(name = "backend-server", contextId = "userRepository", path = "/api/users", configuration = FeignGlobalConfig.class)
 public interface UserRepository {
 	@GetMapping("/me")
+	@RetryWithTokenRefresh
 	UserResponse getMe();
 
 	@PutMapping("/{id}")
+	@RetryWithTokenRefresh
 	void updateUser(@PathVariable("id") Long id, @RequestBody UserUpdateRequest userUpdateRequest);
 }
