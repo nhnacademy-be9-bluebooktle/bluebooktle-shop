23ef3bf1fa143f0b60094fd708cb94326680fc1f
marf3927 <marf3927@gmail.com>
2025-05-30 02:03:58 +0900

    fix(Order): Order id로 구매 목록, 사용 쿠폰, 포장 내역, 주소 데이터 조회하는 API 추가

--- backend-server/src/main/java/shop/bluebooktle/backend/coupon/repository/AbsoluteCouponRepository.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/coupon/repository/AbsoluteCouponRepository.java
@@ -1,8 +1,11 @@
 package shop.bluebooktle.backend.coupon.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import shop.bluebooktle.backend.coupon.entity.AbsoluteCoupon;
 
 public interface AbsoluteCouponRepository extends JpaRepository<AbsoluteCoupon, Long> {
+	Optional<AbsoluteCoupon> findByCouponTypeId(Long couponTypeId);
 }
