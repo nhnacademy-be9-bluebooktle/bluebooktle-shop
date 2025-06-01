f42e2000d9bbd6fba6500852e02a36293551b257
Shin gunsu <gunsu980@gmail.com>
2025-06-02 01:45:32 +0900

    feat : 주문 목록 front작업중 ㅇㅅㅇ

--- frontend-server/src/main/java/shop/bluebooktle/frontend/repository/OrderRepository.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/repository/OrderRepository.java
@@ -5,9 +5,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import shop.bluebooktle.common.domain.order.OrderStatus;
+import shop.bluebooktle.common.dto.common.PaginationData;
 import shop.bluebooktle.common.dto.order.request.OrderCreateRequest;
 import shop.bluebooktle.common.dto.order.response.OrderConfirmDetailResponse;
+import shop.bluebooktle.common.dto.order.response.OrderHistoryResponse;
 import shop.bluebooktle.frontend.config.feign.FeignGlobalConfig;
 
 @FeignClient(url = "${server.gateway-url}", name = "OrderRepository", path = "/api/orders", configuration = FeignGlobalConfig.class)
@@ -19,4 +23,10 @@ public interface OrderRepository {
 	@PostMapping
 	Long createOrder(@RequestBody OrderCreateRequest request);
 
+	@GetMapping("/history")
+	PaginationData<OrderHistoryResponse> getOrderHistory(
+		@RequestParam("page") int page,
+		@RequestParam("size") int size,
+		@RequestParam(value = "status", required = false) OrderStatus status
+	);
 }
