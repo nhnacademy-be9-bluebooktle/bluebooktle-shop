a2a36276ece4b9c9e311d6a7d72e395aceda07d2
sondonghun <ehfk11115@gmail.com>
2025-06-04 17:15:51 +0900

    feat(book): 도서 상세페이지 리뷰 목록 조회

---
+++ backend-server/src/main/java/shop/bluebooktle/backend/book/entity/ReviewLikes.java
@@ -0,0 +1,53 @@
+package shop.bluebooktle.backend.book.entity;
+
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import shop.bluebooktle.common.entity.BaseEntity;
+import shop.bluebooktle.common.entity.auth.User;
+
+@Entity
+@Table(name = "review_likes", uniqueConstraints = {
+	@UniqueConstraint(columnNames = {"user_id", "review_id"})
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EqualsAndHashCode(of = "id", callSuper = false)
+@SQLDelete(sql = "UPDATE `review_likes` SET deleted_at = CURRENT_TIMESTAMP WHERE review_likes_id = ?")
+@SQLRestriction("deleted_at IS NULL")
+public class ReviewLikes extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "review_likes_id")
+	private Long id;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", nullable = false)
+	private User user;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "review_id", nullable = false)
+	private Review review;
+
+	@Builder
+	public ReviewLikes(User user, Review review) {
+		this.user = user;
+		this.review = review;
+	}
+}
