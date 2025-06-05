80a5f86de25d153d8086dd2b06b615940a563a52
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2025-06-05 17:54:20 +0900

    [FIX][Coupon] - 생일 쿠폰 에러 수정 및 쿠폰에서 도서 리스트 변경 (#367)
    * feat : birthday coupon fix
    
    * fix: 도서 리스트 수정에 따른 쿠폰에서의 도서 리스트 반영
    
    * feat: : rebase 병합
    
    * Delete logs/dev/server/backend-server-PORT_IS_UNDEFINED.log

--- backend-server/src/main/java/shop/bluebooktle/backend/coupon/batch/birthday/scheduler/BirthdayCouponScheduler.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/coupon/batch/birthday/scheduler/BirthdayCouponScheduler.java
@@ -1,5 +1,9 @@
 package shop.bluebooktle.backend.coupon.batch.birthday.scheduler;
 
+import java.util.concurrent.TimeUnit;
+
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -13,11 +17,29 @@ import shop.bluebooktle.backend.coupon.batch.birthday.BirthdayCouponLauncher;
 public class BirthdayCouponScheduler {
 
 	private final BirthdayCouponLauncher launcher;
+	private final RedissonClient redissonClient;
 
 	// @Scheduled(cron = "0 0 0 1 * *")
 	@Scheduled(cron = "0 0/5 * * * *") // TODO dev에서 Test를 위함 -> test 후 변경
 	public void runBirthdayCouponJob() {
-		log.info("생일 쿠폰 발급 스케줄러 실행");
-		launcher.run();
+		RLock lock = redissonClient.getLock("lock:BirthdayCouponScheduler");
+		boolean acquired = false;
+
+		try {
+			acquired = lock.tryLock(3, 30, TimeUnit.SECONDS); //최대 3초 대기, 30초 락
+			if (acquired) {
+				log.info("생일 쿠폰 발급 스케줄러 실행");
+				launcher.run();
+			} else {
+				log.info("다른 인스턴스가 처리 중이라 락 걸림");
+			}
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+		} finally {
+			if (acquired && lock.isHeldByCurrentThread()) {
+				lock.unlock();
+				log.info("락 해제");
+			}
+		}
 	}
 }
