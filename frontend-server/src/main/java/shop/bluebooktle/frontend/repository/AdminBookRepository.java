2bde2c0c08139c33753b2a878f44f08b6de49b7b
chossa <130984594+chossa@users.noreply.github.com>
2025-06-08 20:29:21 +0900

    fix(도서 수정): Backend Controller 수정 (#376)

--- frontend-server/src/main/java/shop/bluebooktle/frontend/repository/AdminBookRepository.java
+++ frontend-server/src/main/java/shop/bluebooktle/frontend/repository/AdminBookRepository.java
@@ -17,19 +17,11 @@ import shop.bluebooktle.common.dto.book.response.AdminBookResponse;
 import shop.bluebooktle.common.dto.book.response.AladinBookResponse;
 import shop.bluebooktle.common.dto.book.response.BookAllResponse;
 import shop.bluebooktle.common.dto.book.response.BookCartOrderResponse;
-import shop.bluebooktle.common.dto.book.response.BookInfoResponse;
 import shop.bluebooktle.common.dto.common.PaginationData;
 import shop.bluebooktle.frontend.config.feign.FeignGlobalConfig;
 
-@FeignClient(url = "${server.gateway-url}", name = "BookRepository", path = "/api/books", configuration = FeignGlobalConfig.class)
+@FeignClient(url = "${server.gateway-url}", name = "adminBookRepository", path = "/api/books", configuration = FeignGlobalConfig.class)
 public interface AdminBookRepository {
-
-	@GetMapping
-	PaginationData<BookInfoResponse> getPagedBooks(
-		@RequestParam("page") int page,
-		@RequestParam("size") int size,
-		@RequestParam(value = "searchKeyword", required = false) String searchKeyword
-	);
 
 	@GetMapping("/admin")
 	PaginationData<AdminBookResponse> getPagedBooksByAdmin(
@@ -38,7 +30,7 @@ public interface AdminBookRepository {
 		@RequestParam(value = "searchKeyword", required = false) String searchKeyword
 	);
 
-	@GetMapping("/{bookId}")
+	@GetMapping("/{bookId}/admin")
 	BookAllResponse getBook(@PathVariable("bookId") Long bookId);
 
 	@PutMapping("/{bookId}")
