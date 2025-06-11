b7d4ffd206ab4dd43c60b9bdfd01e3d5ed8700f0
김재혁 <rlawog@nate.com>
2025-06-11 17:10:25 +0900

    refactor: 쿠폰 사용 로직 변경(front -> backend 처리)

--- backend-server/src/main/java/shop/bluebooktle/backend/coupon/service/impl/UserCouponServiceImpl.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/coupon/service/impl/UserCouponServiceImpl.java
@@ -8,13 +8,11 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
-import shop.bluebooktle.backend.coupon.entity.UserCoupon;
 import shop.bluebooktle.backend.coupon.repository.UserCouponRepository;
 import shop.bluebooktle.backend.coupon.service.UserCouponService;
 import shop.bluebooktle.common.domain.coupon.UserCouponFilterType;
 import shop.bluebooktle.common.dto.coupon.response.UsableUserCouponMapResponse;
 import shop.bluebooktle.common.dto.coupon.response.UserCouponResponse;
-import shop.bluebooktle.common.exception.coupon.UserCouponNotFoundException;
 
 @Service
 @RequiredArgsConstructor
@@ -41,21 +39,5 @@ public class UserCouponServiceImpl implements UserCouponService {
 	@Override
 	public Long countExpiringThisMonth(Long userId) {
 		return userCouponRepository.couponExpiringThisMonth(userId);
-	}
-
-	@Override
-	@Transactional
-	public void useCoupon(Long id) {
-		UserCoupon userCoupon = userCouponRepository.findById(id)
-			.orElseThrow(UserCouponNotFoundException::new);
-		userCoupon.useCoupon();
-	}
-
-	@Override
-	@Transactional
-	public void cancelCouponUse(Long id) {
-		UserCoupon userCoupon = userCouponRepository.findById(id)
-			.orElseThrow(UserCouponNotFoundException::new);
-		userCoupon.cancelCoupon();
 	}
 }
