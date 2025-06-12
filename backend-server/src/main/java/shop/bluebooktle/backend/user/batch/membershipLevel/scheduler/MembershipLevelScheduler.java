89b2931a10e8b71f16d8a885ba1933a2bc8abc09
DF_Kim <84631435+Combro-Kim@users.noreply.github.com>
2025-06-12 16:55:11 +0900

    [Test] - 회원 등급 확인을 위한 스케줄러 시간 변경 (#407)
    Co-authored-by: marf3927 <marf3927@gmail.com>

--- backend-server/src/main/java/shop/bluebooktle/backend/user/batch/membershipLevel/scheduler/MembershipLevelScheduler.java
+++ backend-server/src/main/java/shop/bluebooktle/backend/user/batch/membershipLevel/scheduler/MembershipLevelScheduler.java
@@ -14,8 +14,8 @@ public class MembershipLevelScheduler {
 
 	private final MembershipLauncher launcher;
 
-	// @Scheduled(cron = "0 0/1 * * * *") // TODO test 를 위한 1분주기 (dev 테스트 후 제거)
-	@Scheduled(cron = "0 0 3 * * *")
+	// @Scheduled(cron = "0 0 3 * * *")
+	@Scheduled(cron = "0 0 * * * *") // TODO test 를 위한 1시간 주기 (dev 테스트 후 제거)
 	public void runMembershipLevelUpdateJob() {
 		log.info("회원 등급 배치 스케줄러 실행");
 		launcher.run();
