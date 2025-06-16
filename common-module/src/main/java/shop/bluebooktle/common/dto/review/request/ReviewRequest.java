65429c17950433e208cfc99052594eed348b64b7
sondonghun <ehfk11115@gmail.com>
2025-06-17 00:08:30 +0900

    test(book): 리뷰,도서 테스트코드

--- common-module/src/main/java/shop/bluebooktle/common/dto/review/request/ReviewRequest.java
+++ common-module/src/main/java/shop/bluebooktle/common/dto/review/request/ReviewRequest.java
@@ -10,6 +10,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,6 +20,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode(exclude = "imageFiles")
 public class ReviewRequest {
 
 	@NotNull
